423b28b1884ab7f5cf16c0392900d3a3b92ac01b
Israel <isradani.uhp@gmail.com>
2019-10-10 10:52:27 +0200

    maleteo con java script y envio de formulario

--- form.js
+++ form.js
@@ -1,49 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-    form.addEventListener("submit", function(evt) {
-      evt.preventDefault();
-      var form = document.getElementById("form");
-      var nombre = document.getElementById("nombre");
-      var email = document.getElementById("email");
-      var horario = document.getElementById("horario");
-      var ciudad = document.getElementById("city");
-     // var msg = document.getElementById("msg");
-
-      nombre = nombre.value;
-      email = email.value;
-      horario = horario.value;
-      ciudad = ciudad.value;
-      expresion = /\w+@\w+\.[a-z]/;
-
-        if (nombre.length<4){
-         alert ("el nombre es muy corto");
-         return false;
-        }
-        else if(horario.length>20){
-         alert("Horario tener 20 caracteres como maximo");
-         return false;
-       }
-         else if(!expresion.test(email)){
-         alert ("el email no es valido");
-
-         return false;
-        }
-
-        var cajita = document.getElementById("msg");
-        cajita.classList.add("mensaje");
-
-        var form =document.getElementById("form");
-        form.classList.add("sent");
-
-        //document.createElement.classList.add("sent")
+  form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    var form = document.getElementById("form");
+    var nombre = document.getElementById("nombre");
+    var email = document.getElementById("email");
+    var horario = document.getElementById("horario");
+    var ciudad = document.getElementById("city");
 
 
+    nombre = nombre.value;
+    email = email.value;
+    horario = horario.value;
+    ciudad = ciudad.value;
+    expresion = /\w+@\w+\.[a-z]/;
 
-       // this.style.display = "none";
-        // msg.style.display = "block";
-        //style="display:none"
-        msg.focus();
+    if (nombre.length < 4) {
+      alert("el nombre es muy corto");
+      return false;
+    }
+    else if (horario.length > 20) {
+      alert("Horario tener 20 caracteres como maximo");
+      return false;
+    }
+    else if (!expresion.test(email)) {
+      alert("el email no es valido");
 
-    });
+      return false;
+    }
+
+    var cajita = document.getElementById("msg");
+    cajita.classList.add("mensaje");
+
+    var form = document.getElementById("form");
+    form.classList.add("sent");
+    msg.focus();
+
+
+
+    const formData = new FormData(document.getElementById("myForm"));
+    var url = document.getElementById("myForm").action;
+    var metodo = document.getElementById("myForm").method;
+    console.log(formData);
+
+    fetch(url, {
+      method: metodo,
+      body: formData
+    })
+
+      .then(function (respuesta) {
+        console.log("todo ok");
+        console.log(respuesta);
+        respuesta.json();
+        console.log(url);
+        console.log("esta es la respuesta " + respuesta.status);
+      })
+
+
+      .catch(function (respuesta) {
+        console.log("todo mal");
+        console.log(respuesta);
+      });
+
+  });
 
 
 });
